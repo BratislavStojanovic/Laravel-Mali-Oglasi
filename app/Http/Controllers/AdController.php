c9b022353d85cb4ec2774ce8032e2a58d4aa2fea
Bratislav Stojanovic <bata.stojan.83@gmail.com>
2021-03-16 11:06:37 +0100

    Filter - price

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -11,6 +11,8 @@ class AdController extends Controller
     public function index()
     {
 
+        $all_ads = new Ad;
+
          if (isset(request()->cat)) {
 
           //   $cat = Category::where('name',request()->cat)->first();
@@ -21,13 +23,17 @@ class AdController extends Controller
             //  $query->where('name',request()->cat);
                 $query->whereName(request()->cat);
 
-          })->get();
-         }else{
-            $all_ads = Ad::all();
+          });
+         }
+
+         if(isset(request()->type)){
+          
+            $type = (request()->type == 'lower') ? 'asc' : 'desc';
+            $all_ads = $all_ads->orderBy('price',$type);
          }
 
 
-
+        $all_ads = $all_ads->get();
         
         $all_categories = Category::all();
 
@@ -38,7 +44,7 @@ class AdController extends Controller
     {
          $single_ad = Ad::find($id);
 
-        if(auth()->check() && auth()->user()->id !== $single_ad->user_id){
+        if (auth()->check() && auth()->user()->id !== $single_ad->user_id) {
             $single_ad->increment('views');
         }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,15 @@
                     <a href="{{ route('welcome') }}?cat={{ $cat->name}}" class="text-light">{{ $cat->name }}</a>
                  </li>
              @endforeach
+             <li class="list-group-item bg-secondary">
+                 <form action="{{ route('welcome') }}" method="GET">
+                 <select name="type" class="form-control">
+                     <option value="lower" {{(isset(request()->type) && request()->type == 'lower') ? 'selected' : ''}}>Cene rastuce</option>
+                     <option value="upper" {{(isset(request()->type) && request()->type == 'upper') ? 'selected' : ''}}>Cene opadajuce</option>
+                 </select>
+                 <button type="submit" class="btn btn-success form-control mt-1">Search</button>
+                </form>
+             </li>
            </ul>
         </div>
 
